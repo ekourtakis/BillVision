67f396389abe678317c999cf6d841a7c2db17f94
manny <ekourtakis@gmail.com>
2025-02-19 20:50:27 -0500

    changed settings to run on hardware device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 android {
     namespace = "com.example.billvision"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.billvision"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
