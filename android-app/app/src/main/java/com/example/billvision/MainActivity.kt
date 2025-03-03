f00a03be5e3a41b63b6aa3b91af6b97722de47a3
ekourtakis <emmanuel.kourtakis@temple.edu>
2025-03-03 17:25:57 -0500

    added second activity

--- android-app/app/src/main/java/com/example/billvision/MainActivity.kt
+++ android-app/app/src/main/java/com/example/billvision/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.billvision
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.billvision.ui.theme.BillVisionTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +25,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BillVisionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            CameraActivityButtonStuff()
+        }
+    }
+
+    @Composable
+    fun CameraActivityButtonStuff() {
+        val context = LocalContext.current
+        Button(
+            onClick = {
+                startActivity(Intent(context, CameraActivity::class.java))
+            },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Camera")
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BillVisionTheme {
-        Greeting("Android")
-    }
-}
