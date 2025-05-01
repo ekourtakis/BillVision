891905aa75fe2926ca7c819649f85162ce0a2d35
Emmanuel Kourtakis <ekourtakis@gmail.com>
2025-05-01 00:11:03 -0400

    cleaned imports

--- android-app/app/src/main/java/com/example/billvision/MainActivity.kt
+++ android-app/app/src/main/java/com/example/billvision/MainActivity.kt
@@ -8,31 +8,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.example.billvision.activity.CameraActivity
 import com.example.billvision.ui.main.MainScreen
 import com.example.billvision.ui.main.PermissionScreen
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
 
 class MainActivity : ComponentActivity() {
     private val cameraLauncher = registerForActivityResult(
